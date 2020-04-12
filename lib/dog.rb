4a9b17fb40c53f1d6f83baae4b3145b5e7e06591
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-12 03:57:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,23 +3,6 @@ class Dog
     @name = name
     @breed = breed
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def name
-    @breed
-  end
-
 end
 
 #fido = Dog.new(fido, "Mutt")
