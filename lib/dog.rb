c9ffcaa181877163d19cc40174aff2a913ba646c
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-12 03:57:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,23 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
 
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def name
+    @breed
   end
 
 end
